a76e41d69bce3592a435d29d165b83b6f1581324
yoav33333 <yoav.2009.goren@gmail.com>
2025-02-26 12:56:27 +0200

    active intake added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/BadTeleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/BadTeleop.kt
@@ -9,25 +9,24 @@ import dev.frozenmilk.dairy.core.FeatureRegistrar
 import dev.frozenmilk.dairy.core.util.supplier.logical.EnhancedBooleanSupplier
 import dev.frozenmilk.mercurial.Mercurial
 import dev.frozenmilk.mercurial.bindings.BoundBooleanSupplier
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
-import org.firstinspires.ftc.teamcode.commands.extendoCommand
+import dev.frozenmilk.mercurial.commands.groups.Sequential
+import dev.frozenmilk.mercurial.commands.util.Wait
 import org.firstinspires.ftc.teamcode.commands.extendoCommand.extendoMacro
-import org.firstinspires.ftc.teamcode.commands.extendoCommand.regular
-import org.firstinspires.ftc.teamcode.subsystems.BulkReads
 import org.firstinspires.ftc.teamcode.subsystems.armClawSubsystem
-import org.firstinspires.ftc.teamcode.subsystems.clawSubsystem
-import org.firstinspires.ftc.teamcode.subsystems.clawSubsystem.rotateClawL
-import org.firstinspires.ftc.teamcode.subsystems.clawSubsystem.rotateClawR
+
 import org.firstinspires.ftc.teamcode.subsystems.deposit
 import org.firstinspires.ftc.teamcode.subsystems.deposit.intakeSeq
+import org.firstinspires.ftc.teamcode.subsystems.deposit.postIntakeState
 import org.firstinspires.ftc.teamcode.subsystems.deposit.quickRCSimple
-import org.firstinspires.ftc.teamcode.subsystems.deposit.switchArmOut
 import org.firstinspires.ftc.teamcode.subsystems.extendoSubsystem
 import org.firstinspires.ftc.teamcode.subsystems.followerSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.intakeSubsystem
 import org.firstinspires.ftc.teamcode.subsystems.linearSlides
 import org.firstinspires.ftc.teamcode.subsystems.linearSlides.getPose
-import org.firstinspires.ftc.teamcode.subsystems.linearSlides.goToBasket
+import org.firstinspires.ftc.teamcode.subsystems.linearSlides.goToHighBasket
 import org.firstinspires.ftc.teamcode.subsystems.linearSlides.goToHighChamber
+import org.firstinspires.ftc.teamcode.subsystems.linearSlides.goToLowBasket
+import org.firstinspires.ftc.teamcode.subsystems.linearSlides.goToLowChamber
 import org.firstinspires.ftc.teamcode.subsystems.linearSlides.magneticLimit
 import org.firstinspires.ftc.teamcode.subsystems.linearSlides.resetHeight
 import org.firstinspires.ftc.teamcode.subsystems.linearSlides.target
@@ -41,17 +40,18 @@ class BadTeleop : MegiddoOpMode() {
     lateinit var telemetryDB: MultipleTelemetry
     var lastRunTime = 0.0
 
+    //TODO: add auto closing if pixel intaked
     override fun myInit() {
         //operator controls
         Mercurial.gamepad2.options.onTrue(deposit.armIn)
-        Mercurial.gamepad2.y.whileTrue(clawSubsystem.changeClawPos)
-        Mercurial.gamepad2.b.onTrue(extendoCommand.noTransfer)
-        Mercurial.gamepad2.leftStickButton.onTrue(clawSubsystem.resetAngleClaw)
-        Mercurial.gamepad2.a.onTrue(extendoCommand.notOpen)
-        Mercurial.gamepad2.leftBumper.onTrue(rotateClawL)
-        Mercurial.gamepad2.rightBumper.onTrue(rotateClawR)
-        Mercurial.gamepad2.x.onTrue(regular)
-        Mercurial.gamepad2.dpadUp.onTrue(goToBasket)
+        Mercurial.gamepad2.y.onTrue(intakeSubsystem.changeClawPos)
+        Mercurial.gamepad2.b.onTrue(deposit.changeClawPos)
+//        Mercurial.gamepad2.leftStickButton.onTrue(clawSubsystem.resetAngleClaw)
+        Mercurial.gamepad2.a.onTrue(intakeSeq)
+        Mercurial.gamepad2.leftBumper.whileTrue(intakeSubsystem.intake)
+        Mercurial.gamepad2.rightBumper.whileTrue(intakeSubsystem.outtake)
+        Mercurial.gamepad2.x.onTrue(extendoMacro)
+        Mercurial.gamepad2.dpadUp.onTrue(goToHighBasket)
         Mercurial.gamepad2.dpadDown.onTrue(goToHighChamber)
         Mercurial.gamepad2.leftStickButton.onTrue(linearSlides.goToPreset(0.0).with(deposit.halfArmIn))
 //        Mercurial.gamepad2.dpadLeft .onTrue(goToLowBasket)
@@ -68,18 +68,25 @@ class BadTeleop : MegiddoOpMode() {
             .whileTrue(resetHeight)
 
         //drive controls
-//        BoundBooleanSupplier(EnhancedBooleanSupplier { Mercurial.gamepad1.leftTrigger.state >0.2 })
-//            .whileTrue(followerSubsystem.runRobotCentric)
-        Mercurial.gamepad1.rightBumper.onTrue(linearSlides.switchBasket)
-//        BoundBooleanSupplier(EnhancedBooleanSupplier{Mercurial.gamepad1.rightTrigger.state>0.2})
-//            .onTrue(followerSubsystem.secondGear)
-//            .onFalse(followerSubsystem.firstGear)
+        BoundBooleanSupplier(EnhancedBooleanSupplier { Mercurial.gamepad1.rightTrigger.state >0.2 })
+            .onTrue(followerSubsystem.secondGear)
+            .onFalse(followerSubsystem.firstGear)
+        Mercurial.gamepad1.rightBumper.onTrue(followerSubsystem.secondGear)
+            .onFalse(followerSubsystem.firstGear)
+        BoundBooleanSupplier(EnhancedBooleanSupplier{Mercurial.gamepad1.leftTrigger.state>0.2})
+            .onTrue(followerSubsystem.secondGear)
+            .onFalse(followerSubsystem.firstGear)
         Mercurial.gamepad1.rightStickButton.onTrue(deposit.changeClawPos)
-//        Mercurial.gamepad1.a.onTrue(followerSubsystem.changeCentric)
+
         Mercurial.gamepad1.dpadUp.onTrue( followerSubsystem.angleReset)
         Mercurial.gamepad1.leftBumper.onTrue( followerSubsystem.angleReset)
-        Mercurial.gamepad1.leftStickButton.onTrue(switchArmOut)
-        Mercurial.gamepad1.a.onTrue(extendoCommand.toggleFlip)
+        Mercurial.gamepad1.leftStickButton.onTrue(
+            Sequential(
+                deposit.closeH,
+                Wait(0.3),
+                postIntakeState
+            )
+        )
 
         telemetryDB = MultipleTelemetry(
             FeatureRegistrar.activeOpMode.telemetry,
@@ -90,15 +97,13 @@ class BadTeleop : MegiddoOpMode() {
 
     override fun myStart() {
         extendoMacro.schedule()
+        followerSubsystem.teleopDrive.schedule()
     }
     override fun myLoop() {
-        followerSubsystem.teleopDrive.schedule()
-        telemetryDB.addData("current Control", BulkReads.modules[0].getCurrent(CurrentUnit.AMPS))
-        telemetryDB.addData("current Expention", BulkReads.modules[1].getCurrent(CurrentUnit.AMPS))
         telemetryDB.addData("delta time", runtime - lastRunTime)
         lastRunTime = runtime
         telemetryDB.addData("clawPosDepo", deposit.depoClawServo.position)
-        telemetryDB.addData("clawPos", clawSubsystem.clawServo.position)
+//        telemetryDB.addData("clawPos", clawSubsystem.clawServo.position)
         telemetryDB.addData("v4b", armClawSubsystem.armClawServo.position)
         telemetryDB.addData("v4b flip", armClawSubsystem.angleClawServo.position)
         telemetryDB.addData("ex r", extendoSubsystem.extendoServoR.position)
@@ -106,20 +111,14 @@ class BadTeleop : MegiddoOpMode() {
         telemetryDB.addData("offset", linearSlides.offset)
         telemetryDB.addData("sensor", magneticLimit.state)
         telemetryDB.addData("sch", Mercurial.isScheduled(linearSlides.runToPosition))
-        telemetryDB.addData("leftCenter", linearSlides.leftCenter.power)
-        telemetryDB.addData("leftSide", linearSlides.leftSide.power)
-        telemetryDB.addData("rightSide", linearSlides.rightSide.power)
-        telemetryDB.addData("rightCenter", linearSlides.rightCenter.power)
-        telemetryDB.addData("leftCenterPose", linearSlides.leftCenter.currentPosition)
-        telemetryDB.addData("leftSidePose", linearSlides.leftSide.currentPosition)
-        telemetryDB.addData("rightSidePose", linearSlides.rightSide.currentPosition)
-        telemetryDB.addData("rightCenterPose", linearSlides.rightCenter.currentPosition)
-        telemetryDB.addData("rotate", clawSubsystem.clawRotationServo.position)
+        telemetryDB.addData("l1", linearSlides.motorLiftNear.power)
+        telemetryDB.addData("l2", linearSlides.motorLiftMiddle.power)
+        telemetryDB.addData("l3", linearSlides.motorLiftFar.power)
+//        telemetryDB.addData("rotate", clawSubsystem.clawRotationServo.position)
         telemetryDB.addData("pose", getPose())
         telemetryDB.addData("target", target)
         telemetryDB.addData("error", target - getPose())
         telemetryDB.addData("deposit claw", deposit.depoClawServo.position)
-//        telemetryDB.addData("diff", linearSlides.getPoseRight()-linearSlides.getPoseLeft())
         telemetryDB.update()
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/armClawSubsystem.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/armClawSubsystem.kt
@@ -37,7 +37,7 @@ object armClawSubsystem : Subsystem {
         s
     }
     val angleClawServo: Servo by OpModeLazyCell {
-        val s =
+        val s = 
             FeatureRegistrar.activeOpMode.hardwareMap.get(
                 Servo::class.java, "arm s"
 
@@ -46,19 +46,17 @@ object armClawSubsystem : Subsystem {
         s
     }
     @JvmField
-    var armOut = 0.1
+    var armOut = 0.0
     @JvmField
-    var armIn = 0.39
-//    @JvmField
-//    var armInFull = 1.0
+    var armIn = 0.98
+    var armInFull = 1.0
     @JvmField
-    var transfareState = 0.11
+    var transfareState = 0.2
+    val postTransfareState = 0.6
     @JvmField
-    var postTransfareState = 0.3
-    @JvmField
-    var intakeState = 1.0
+    var intakeState = 0.75
     override fun preUserInitHook(opMode: Wrapper) {
-         armOut =  if (activeOpModeWrapper.opModeType == OpModeMeta.Flavor.TELEOP) 0.16  else 0.15
+         armOut =  if (activeOpModeWrapper.opModeType == OpModeMeta.Flavor.TELEOP) 0.035 else 0.046
     }
 
     val angleTransfer = Lambda("angleTransfer")
@@ -77,37 +75,26 @@ object armClawSubsystem : Subsystem {
         .setInit {
             armClawServo.setPosition(armOut)
         }
-    val moveArmOutFull = Lambda("moveArmOutFull")
-        .setInit {
-            armClawServo.position = armOut-0.02
-        }
     val moveArmIn = Lambda("moveArmIn")
         .setInit {
             armClawServo.setPosition(armIn)
         }
-//    val moveArmInFull = Lambda("moveArmInFull")
-//        .setInit {
-//            armClawServo.setPosition(armInFull)
-//        }
-    @JvmStatic
-    val extendoPushState = Lambda("eps")
-        .setInit{
-            armClawServo.position = 0.0
-            angleClawServo.position = 0.8
+    val moveArmInFull = Lambda("moveArmInFull")
+        .setInit {
+            armClawServo.setPosition(armInFull)
         }
+
+
     val openClawArm = Sequential(
         moveArmOut,
-//        Wait(0.2),
+        Wait(0.2),
         angleIntake
     )
     val closeClawArm = Sequential(
-        moveArmIn,
-        Wait(0.12),
-        angleTransfer
+        angleTransfer,
+        Wait(0.2),
+        moveArmIn
     )
-    @JvmStatic
-    val armUp = Lambda("au")
-        .setInit{ armClawServo.position = 0.15}
 
 
 }
